bf45cfea41800c513d7657babf24b65a39d2bd72
Alexander Tsarapkine <alexandertsarapkine@Alexanders-Laptop.local>
2023-07-20 18:35:51 -0400

    initial spring app going

---
+++ server/src/main/java/com/example/server/ServerController.java
@@ -0,0 +1,14 @@
+package com.example.server;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServerController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
