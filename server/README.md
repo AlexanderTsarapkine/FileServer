cca2f6bbe17846ffb1a61260dcb544674d4d6834
Alexander Tsarapkine <alexandertsarapkine@v1040-wn-rt-a-51-135.campus-dynamic.uwaterloo.ca>
2023-07-25 21:07:52 -0400

    converted to spring data rest

---
+++ server/README.md
@@ -0,0 +1,5 @@
+from the ./server directory, execute the following to run the program:
+    ./mvnw spring-boot:run
+
+followed:
+    https://www.baeldung.com/spring-data-rest-intro
---
+++ server/src/main/java/com/example/server/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.server;
+
+import org.springframework.data.repository.CrudRepository;
+
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "users", path = "users") // optional
+// ^ used to customize REST endpoint, if omitted endpoint would be /websiteUsers instead of /users
+    public interface UserRepository extends CrudRepository<WebsiteUser, Long> {
+
+    List<WebsiteUser> findByName(@Param("name") String name);
+}
