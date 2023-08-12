c19c34f67b11235b065f27d87898c294c36f3aed
Alexander Tsarapkine <alexandertsarapkine@alexandrslaptop.home>
2023-08-12 18:15:56 -0400

    Added initial oauth to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from "./components/login";
+import Logout from "./components/logout";
+import { useEffect } from 'react';
+import {gapi} from 'gapi-script';
+
+const clientId = process.env.REACT_APP_CLIENT_ID;
 
 function App() {
+
+  useEffect(() => {
+    function start() {
+      gapi.client.init({
+        clientId: clientId,
+        scope: ""
+      })
+    };
+
+    gapi.load('client:auth2', start);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Login/>
+      <Logout/>
     </div>
   );
 }
